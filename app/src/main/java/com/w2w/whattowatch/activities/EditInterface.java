7799bcd327a4e316e3f1331eb0a9bb563a6a234d
mjgalindo <mjgalindoramos@gmail.com>
2016-03-28 16:40:38 +0200

    Implement List and Edit interfaces

--- app/src/main/java/com/w2w/whattowatch/activities/EditInterface.java
+++ app/src/main/java/com/w2w/whattowatch/activities/EditInterface.java
@@ -1,7 +1,20 @@
 package com.w2w.whattowatch.activities;
 
 /**
- * Created by Sergio on 28/03/2016.
+ * Interface for activities that allow information inputs from the user and later writes them to
+ * a database.
  */
 public interface EditInterface {
+
+    /**
+     * Saves all user inputs to the database
+     */
+    void saveState();
+
+    /**
+     * Fills all user input fields with previously existing information from the database.
+     * @param elementId id of the element which information will be retrieved to the input
+     *      fields from the database.
+     */
+    void populateFields(long elementId);
 }
--- app/src/main/java/com/w2w/whattowatch/activities/ListInterface.java
+++ app/src/main/java/com/w2w/whattowatch/activities/ListInterface.java
@@ -1,7 +1,32 @@
 package com.w2w.whattowatch.activities;
 
 /**
- * Created by Sergio on 28/03/2016.
+ * Interface for activities that show and modify elements from a database
  */
 public interface ListInterface {
+    /**
+     * Fetches and shows the elements corresponding to the class that implements this
+     * interface.
+     */
+    void list();
+
+    /**
+     * Starts an activity to create an element corresponding to the class that implements this
+     * interface.
+     */
+    void create();
+
+    /**
+     * Starts an activity to edit an element corresponding to the class that implements this
+     * interface.
+     * @param elementId id of the element that will be edited
+     */
+    void edit(long elementId);
+
+    /**
+     * Deletes an element corresponding to the class that implements this
+     * interface.
+     * @param elementId id of the element that will be deleted
+     */
+    void delete(long elementId);
 }
