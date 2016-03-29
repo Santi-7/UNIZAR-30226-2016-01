3c185e7b6cb97070983b6f14cad03652562e0354
Santiago Gil <santi.gil.begue7@gmail.com>
2016-03-29 17:35:43 +0200

    Implement CreateSeries

--- app/src/main/java/com/w2w/whattowatch/data/DbAdapter.java
+++ app/src/main/java/com/w2w/whattowatch/data/DbAdapter.java
@@ -1,5 +1,6 @@
 package com.w2w.whattowatch.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,9 +13,10 @@ import android.util.Log;
  */
 public class DbAdapter {
 
-    /*Database constants*/
+    /* Database constants */
     public static final String SERIES_KEY_ID = "s_id"; // A series' unique id field
     public static final String SERIES_KEY_TITLE = "title"; // Series title field
+    public static final String SERIES_KEY_DESCRIPTION = "description"; // Series description field
 
     public static final String EPISODE_KEY_ID = "e_id"; // An episode's unique id field
     public static final String EPISODE_KEY_NAME = "name"; // Episode name field
@@ -22,7 +24,12 @@ public class DbAdapter {
     public static final String EPISODE_KEY_EPISODE_NUM = "number"; // Episode number field
     public static final String EPISODE_KEY_SERIES = "series"; // Episode series field
 
-    /* Adapter private variables*/
+    private static final String DATABASE_NAME = "data";
+    private static final String DATABASE_SERIES_TABLE = "series"; // Table of series
+    private static final String DATABASE_EPISODES_TABLE = "episodes"; // Table of episodes
+    private static final int DATABASE_VERSION = 2;
+
+    /* Adapter private variables */
     private Context ctx;
     private SQLiteDatabase sDb;
     private SeriesDatabaseHelper sDbHelper;
@@ -40,7 +47,7 @@ public class DbAdapter {
      * If it can't be created an exception will be thrown.
      * @return this DbAdapter // TODO: Check this is correct, do we need this return?
      */
-    public DbAdapter open(){
+    public DbAdapter open() {
         sDbHelper = new SeriesDatabaseHelper(ctx);
         sDb = sDbHelper.getWritableDatabase(); // Gets the database (either retrieved or created)
         sDb.execSQL("PRAGMA foreign keys = ON;"); // Allows using foreign keys
@@ -50,19 +57,8 @@ public class DbAdapter {
     /**
      * Closes the database helper. // Bug:
      */
-    public void close(){
+    public void close() {
         sDbHelper.close();
-    }
-
-    /**
-     * Creates a new series in the database.
-     * @param title of the series to be created.
-     * @return id of the newly created series in the database.
-     * // TODO: Decide whether -1 or an exception will be return if it fails
-     */
-    public long createSeries(String title){
-        // TODO: Implement createSeries()
-        return 0;
     }
 
     //////////////////////////////////////////////////////////////
@@ -70,12 +66,28 @@ public class DbAdapter {
     //////////////////////////////////////////////////////////////
 
     /**
+     * Creates a new series in the database.
+     * @param title of the series to be created.
+     * @return id of the newly created series in the database.
+     * // TODO: Decide whether -1 or an exception will be return if it fails
+     */
+    public long createSeries(String title, String description) {
+        if (title == null || title.equals("") || description == null) {
+            return -1;
+        }
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(SERIES_KEY_TITLE, title);
+        initialValues.put(SERIES_KEY_DESCRIPTION, description);
+        return sDb.insert(DATABASE_SERIES_TABLE, null, initialValues);
+    }
+
+    /**
      * Updates the title of an existing series.
      * @param title new title for the series.
      * @param seriesId of the series that will be updated.
      * @return true if and only if the series title could be updated.
      */
-    public boolean updateSeries(String title, long seriesId){
+    public boolean updateSeries(String title, long seriesId) {
         // TODO: Implement updateSeries()
         return false;
     }
@@ -85,7 +97,7 @@ public class DbAdapter {
      * @param seriesId id of the series that will be deleted.
      * @return true if and only if the series could be deleted.
      */
-    public boolean deleteSeries(long seriesId){
+    public boolean deleteSeries(long seriesId) {
         // TODO: implement deleteSeries()
         return false;
     }
@@ -95,7 +107,7 @@ public class DbAdapter {
      * @param seriesId id of the series to be retrieved.
      * @return Cursor positioned at the series with id [seriesId]
      */
-    public Cursor fetchSeries(long seriesId){
+    public Cursor fetchSeries(long seriesId) {
         //TODO: implement fetchSeries()
         return null;
     }
@@ -104,7 +116,7 @@ public class DbAdapter {
      * Retrieves all series from the database.
      * @return Cursor positioned at the head of all the series in the database.
      */
-    public Cursor fetchAllSeries(){
+    public Cursor fetchAllSeries() {
         // TODO: implement fetchAllSeries()
         return null;
     }
@@ -117,7 +129,7 @@ public class DbAdapter {
      * @param series the new episode belongs to.
      * @return id of the new episode
      */
-    public long createEpisode(String name, int season, int number, long series){
+    public long createEpisode(String name, int season, int number, long series) {
         // TODO: implement createEpisode()
         return 0;
     }
@@ -135,7 +147,7 @@ public class DbAdapter {
      * @param episodeId of the episode that will be updated.
      * @return true if and only if the episode was updated correctly.
      */
-    public boolean updateEpisode(String name, int season, int number, long series, long episodeId){
+    public boolean updateEpisode(String name, int season, int number, long series, long episodeId) {
         //TODO: implement updateEpisode()
         return false;
     }
@@ -145,7 +157,7 @@ public class DbAdapter {
      * @param episodeId id of the episode that will be deleted.
      * @return true if and only if the episode could be deleted from the database.
      */
-    public boolean deleteEpisode(long episodeId){
+    public boolean deleteEpisode(long episodeId) {
         //TODO: implement deleteEpisode()
         return false;
     }
@@ -155,7 +167,7 @@ public class DbAdapter {
      * @param episodeId id of the episode to be retrieved.
      * @return Cursor positioned at the episode with id [episodeId]
      */
-    public Cursor fetchEpisode(long episodeId){
+    public Cursor fetchEpisode(long episodeId) {
         //TODO: implement fetchEpisode()
         return null;
     }
@@ -165,7 +177,7 @@ public class DbAdapter {
      * @param series id of the series which episodes will be returned
      * @return Cursor positioned at the head of all the episodes of the series in the database.
      */
-    public Cursor fetchAllEpisodes(long series){
+    public Cursor fetchAllEpisodes(long series) {
         //TODO: implement fetchEpisode()
         return null;
     }
