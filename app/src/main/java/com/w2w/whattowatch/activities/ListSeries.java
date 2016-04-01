feeb2429ce5d1408595b08d4f4cce27ee477892c
mjgalindo <mjgalindoramos@gmail.com>
2016-04-01 09:31:54 +0200

    Implement basic navigation to create new Series

--- app/src/main/java/com/w2w/whattowatch/activities/ListSeries.java
+++ app/src/main/java/com/w2w/whattowatch/activities/ListSeries.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -65,8 +66,12 @@ public class ListSeries extends AppCompatActivity implements ListInterface {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.create_new_series:
+                create();
+                return true;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -96,6 +101,7 @@ public class ListSeries extends AppCompatActivity implements ListInterface {
      * Starts an activity to create a new series
      */
     public void create() {
+        Log.d("ListSeries", "Create new series");
         Intent i = new Intent(this, EditSeries.class);
         startActivityForResult(i, ACTIVITY_CREATE);
     }
