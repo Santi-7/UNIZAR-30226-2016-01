929442986e6c5ae533cf7fed654df9a8e2c86d59
mjgalindo <mjgalindoramos@gmail.com>
2016-04-03 20:21:56 +0200

    Prepare ListEpisodes for future work

--- app/src/main/java/com/w2w/whattowatch/activities/ListEpisodes.java
+++ app/src/main/java/com/w2w/whattowatch/activities/ListEpisodes.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +29,12 @@ public class ListEpisodes extends AppCompatActivity implements ListInterface{
                         .setAction("Action", null).show();
             }
         });
+
+        long seriesId = (Long) this.getIntent().getExtras().get("sid");
+        Log.d("ListEpisodes: ", "Started for series: " + seriesId);
+        //TODO: Implement UI, this includes using fragments.
+        //TODO: Retrieve info for seriesId and fill the UI
+
     }
 
     @Override
